1923f543d1fd363e226f19804f99899b819b0b62
Rajne19 <102701059+Rajne19@users.noreply.github.com>
2022-06-02 04:26:52 +0200

    Add files via upload

---
+++ Webbie/web.php
@@ -0,0 +1,150 @@
+<head>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<?php require_once("bijhorend/header.php");?>
+
+<div class="d-flex p-4"></div>
+<h3 class="text-center">Webbie, De juiste plek voor al je elektronica</h3>	
+<div class="container">
+   <div class="row">
+   
+      <img class="d-block w-100" src="images/test.jpg" alt="Second slide" style="max-height: 700px;">
+   </div>
+</div>
+
+
+
+
+<div class="d-flex p-2"></div>
+<main role="main">
+   <section class="jumbotron text-center">
+      <div class="container">
+         <h1>Webbie</h1>
+         <p class="lead text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+         <p>
+            <a href="#" class="btn btn-primary my-2">Meer info</a>
+         </p>
+      </div>
+   </section>
+   
+   
+
+   <div class="d-flex p-4"></div>
+   <div class="container">
+      <div class="row">
+         <div class="col-sm">
+            <figure class="figure">
+               <h3 class="text-center">Ruime assortiment</h3>
+               <img src="images/ex1.png" class="figure-img img-fluid rounded"  alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+         <div class="col-sm">
+            <figure class="figure">
+               <h3 class="text-center">Betaal veilig</h3>
+               <img src="images/ex2.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+         <div class="col-sm">
+            <figure class="figure">
+               <h3 class="text-center">24/7 Support </h3>
+               <img src="images/ex3.png" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+      </div>
+   </div>
+   <div class="d-flex p-2"></div>
+      <div class="container">
+      <h3 class="text-center">Ruime assortiment</h3>
+      <div class="row">
+         
+         <div class="col-xl">
+            <figure class="figure">
+               <img src="images/ph1.png" class="figure-img img-fluid rounded"  style="max-height: 300px;" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+         <div class="col-xl">
+            <figure class="figure">
+               <img src="images/ph1.png" class="figure-img img-fluid rounded"  style="max-height: 300px;" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+         <div class="col-xl">
+            <figure class="figure">
+               <img src="images/ph1.png" class="figure-img img-fluid rounded"  style="max-height: 300px;" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+         <div class="col-xl">
+            <figure class="figure">
+               <img src="images/ph1.png" class="figure-img img-fluid rounded"  style="max-height: 300px;" alt="A generic square placeholder image with rounded corners in a figure.">
+            </figure>
+         </div>
+      </div>
+   </div>
+   
+   <section class="p-2 p-md-5 text-center text-lg-start shadow-1-strong rounded" style="
+      background-image: url(https://mdbcdn.b-cdn.net/img/Photos/Others/background2.webp);
+      ">
+      <h2 class="text-center">Onze team</h2>
+      <div class="row d-flex justify-content-center">
+         <div class="col-md-10">
+            <div class="card">
+               <div class="card-body m-3">
+                  <div class="row">
+                     <div class="col-lg-4 d-flex justify-content-center align-items-center mb-4 mb-lg-0">
+                        <img src="images/worker1.png"
+                           class="rounded-circle img-fluid shadow-1" alt="woman avatar" width="200" height="200" />
+                     </div>
+                     <div class="col-lg-8">
+                        <p class="text-muted fw-light mb-4">
+                           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente
+                           molestiae numquam quas, voluptates omnis nulla ea odio quia similique
+                           corrupti magnam.
+                        </p>
+                        <p class="fw-bold lead mb-2"><strong>Neeraj </strong></p>
+                        <p class="fw-bold text-muted mb-0">Product manager</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="p-2 p-md-5 text-center text-lg-start shadow-1-strong rounded" style="
+      background-image: url(https://mdbcdn.b-cdn.net/img/Photos/Others/background2.webp);
+      ">
+      <div class="row d-flex justify-content-center">
+         <div class="col-md-10">
+            <div class="card">
+               <div class="card-body m-3">
+                  <div class="row">
+                     <div class="col-lg-8 ">
+                        <p class="text-muted fw-light mb-4">
+                           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente
+                           molestiae numquam quas, voluptates omnis nulla ea odio quia similique
+                           corrupti magnam.
+                        </p>
+                        <p class="fw-bold lead mb-2"><strong>Rojhat Yildirim</strong></p>
+                        <p class="fw-bold text-muted mb-0">Product manager</p>
+                     </div>
+                     <div class="col-lg-4">
+                        <img src="images/worker2.png"
+                           class="rounded-circle img-fluid shadow-1" alt="woman avatar" width="200" height="200" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   
+   
+</main>
+<footer class="text-muted">
+<div class="container">
+   <p class="float-right">
+      <a href="#">Back to top</a>
+   </p>
+   <p>Webbie 2022</a>.</p>
+</div>
+<br> <br>
+<?php require_once("bijhorend/footer.php");?>
